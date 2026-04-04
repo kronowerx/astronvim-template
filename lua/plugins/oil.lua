552ea62a3663ee1d0118d586476594183587a3d3
jp-hong <51396562+jp-hong@users.noreply.github.com>
2026-04-04 16:35:12 +0900

    add my plugins

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    watch_for_changes = true,
+    view_options = { show_hidden = true },
+    float = {
+      max_height = 0.8,
+      max_width = 0.8
+    }
+  },
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  config = function(_, opts)
+    require('oil').setup(opts)
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  end
+}
---
+++ lua/plugins/pyright.lua
@@ -0,0 +1,34 @@
+return {
+    -- require('lspconfig').pyright.setup {
+    --     settings = {
+    --         pyright = {
+    --             -- Using Ruff's import organizer
+    --             disableOrganizeImports = true,
+    --         },
+    --         python = {
+    --             analysis = {
+    --                 -- Ignore all files for analysis to exclusively use Ruff for linting
+    --                 ignore = { '*' },
+    --             },
+    --         },
+    --     },
+    -- }
+
+    vim.lsp.config(
+        'pyright', {
+            settings = {
+                pyright = {
+                    -- Using Ruff's import organizer
+                    disableOrganizeImports = true,
+                },
+                python = {
+                    analysis = {
+                        -- Ignore all files for analysis to exclusively use Ruff for linting
+                        ignore = { '*' },
+                    },
+                },
+            },
+        }
+    ),
+    vim.lsp.enable('pyright')
+}
