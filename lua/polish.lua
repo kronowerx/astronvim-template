836e0370174264b36fecd48955c97370c45d6944
JP Hong <51396562+kronowerx@users.noreply.github.com>
2026-04-06 12:19:09 +0900

    Update polish.lua

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,4 +1,19 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Spell check
+vim.o.spell = false
+
+-- Block cursor
+vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
+-- Tab
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
+
+-- Disable mouse
+vim.o.mouse = ''
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
