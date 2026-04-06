cdf65702745404a1f9dd70502ed224c18abbf2a2
JP Hong <51396562+kronowerx@users.noreply.github.com>
2026-04-06 10:41:46 +0900

    Update go.lua

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,23 +1,28 @@
--- go
+---@type LazySpec
 return {
-  'ray-x/go.nvim',
-  dependencies = { -- optional packages
-    'ray-x/guihua.lua', 'neovim/nvim-lspconfig', 'nvim-treesitter/nvim-treesitter' },
-  opts = {},
-  config = function(_, opts)
-    require('go').setup(opts)
+  "ray-x/go.nvim",
+  dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = function()
 
-    -- Run gofmt + goimports on save
-    local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+    require("go").setup(opts)
+    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*.go",
       callback = function()
-        require('go.format').goimports()
+      require('go.format').goimports()
       end,
       group = format_sync_grp,
     })
+    return {
+      -- lsp_keymaps = false,
+      -- other options
+    }
   end,
-  event = { 'CmdlineEnter' },
-  ft = { 'go', 'gomod' },
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
   build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
 }
