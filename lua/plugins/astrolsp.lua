c8311f700f74eea088e16572575cf6b6618d380d
j-p-hong <j.p.hong@samsung.com>
2026-07-03 17:04:39 +0900

    fix format on save

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -31,7 +31,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
-        "gopls", -- let conform (goimports/gofumpt/gci) own Go formatting
+        "gopls",
+        "pyrefly", -- let conform own python formatting
       },
       timeout_ms = 3000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,10 @@ return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
     opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    }
 
     opts.formatters_by_ft.go = {
       "goimports",
