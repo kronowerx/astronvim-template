07a18b9fc5fea896b2d084219acbcfb8884dc119
j-p-hong <j.p.hong@samsung.com>
2026-07-03 16:03:57 +0900

    update config

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,17 +1,17 @@
 -- zen mode
 return {
-  'folke/zen-mode.nvim',
+  "folke/zen-mode.nvim",
   config = function()
-    require('zen-mode').setup {
+    require("zen-mode").setup {
       window = {
         backdrop = 0.85,
         width = 120,
       },
       plugins = {
         options = { laststatus = 3 },
-        alacritty = { enabled = true, font = '12' },
+        alacritty = { enabled = true, font = "13" },
       },
     }
-    vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', { desc = 'Toggle [Z]enMode' })
+    vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", { desc = "Toggle [Z]enMode" })
   end,
 }
